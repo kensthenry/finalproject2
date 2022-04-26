c58d04be4e88cfc8e2c816797148c0808b7c5e2b
Kensley <kensley3@gmail.com>
2022-04-26 08:45:15 -0400

    Merge branch 'main' of https://github.com/kensthenry/lastwebstore into main

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
-    class Article < ApplicationRecord
         validates :title, presence: true
         validates :body, presence: true, length: { minimum: 10 }
+        validates :price, comparison: {greater_than: 0}
+        validates :stock, comparison: {greater_than_or_equal_to: 0}
       end
-      
+    end
+  end
 end
